1ab084c56ad1894a0f8bb301f58696cfb0beffb0
Tthaodangiu <150150128+Tthaodangiu@users.noreply.github.com>
2023-11-18 03:22:01 -0800

    Update exercise70.py

--- exercise70.py
+++ exercise70.py
@@ -3,9 +3,9 @@ k = int(input("Enter the shift amount: "))
 result = ""
 for i in range(len(text)):
     char = text[i]
-    if "A"<=char<="Z": #Chữ in hoa
+    if "A"<=char<="Z":
         result = result + chr((ord(char) + k-65) % 26 + 65) 
-    elif 97<=ord(char)<=122: #Chữ thường
+    elif 97<=ord(char)<=122:
         result = result + chr((ord(char) + k - 97) % 26 + 97)
     else:
         result = result + char
